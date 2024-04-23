86776f59580dbff699ff969e6c4aa5b9c5f6a941
kibi11 <arpansinghranchi@gmail.com>
2024-04-24 04:26:19 +0530

    add animation, image , fixed messaging sys

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,16 @@
 import { useForm } from "react-hook-form";
 import "./Contact.css";
 import { init, sendForm } from "emailjs-com";
-import { useState } from "react";
+import { useState, useRef } from "react";
 init("user_2SDEeL0UL6Jv6ZiqOldAI");
 
 const Contact = ({ handleAnimate, cardAnime }) => {
   const [statusMessage, setStatusMessage] = useState("Message");
   const [contactNumber, setContactNumber] = useState("000000");
   const [closeButton, setCloseButton] = useState("false");
+
+  //FORM REF
+  const messageForm = useRef();
 
   const generateContactNumber = () => {
     const numStr = "000000" + ((Math.random() * 1000000) | 0);
@@ -21,15 +24,19 @@ const Contact = ({ handleAnimate, cardAnime }) => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-
+  const onSubmit = ({ user_name, user_email, message }) => {
+    console.log({ user_name, user_email, message });
     /// generate the number and send the email to the form owner.
     const form = document.querySelector("#contact-form");
     const statusMessage = document.querySelector(".status-message");
 
     generateContactNumber();
-    sendForm("service_zu2aiyd", "template_kcxezhj", "#contact-form").then(
+    sendForm(
+      "service_sg771vd",
+      "template_13sef71",
+      "#contact-form",
+      "LQKREMXHW2n9ujYQZ"
+    ).then(
       function (response) {
         console.log("SUCCESS!", response.status, response.text);
         setStatusMessage("I'll get back to you shortly");
@@ -70,42 +77,47 @@ const Contact = ({ handleAnimate, cardAnime }) => {
       <div className="responsiveContact">Contact</div>
       <p className="status-message">{statusMessage}</p>
       <form
-        className="formOuter"
+        className="form-container"
         onSubmit={handleSubmit(onSubmit)}
         id="contact-form"
+        ref={messageForm}
       >
-        <input type="hidden" name="contact_number" value={contactNumber} />
-        <input
-          className="formInput"
-          type="text"
-          placeholder="Your Name"
-          name="user_name"
-          defaultValue="Anonymous"
-          {...register("user_name")}
-        ></input>
-        {errors.user_email && <p style={styleObj}>This field is required</p>}
-        <input
-          className="formInput"
-          type="email"
-          placeholder="Your E-mail"
-          name="user_email"
-          {...register("user_email", { required: true })}
-        ></input>
-        <textarea
-          className="formInput2"
-          name="Text1"
-          cols="30"
-          rows="5"
-          placeholder="Message"
-          name="message"
-          {...register("message")}
-          maxLength="1500"
-        ></textarea>
-        {message.length !== 0 ? (
-          <p className="message-chars-left">
-            {`${500 - message.length} chars left`}
-          </p>
-        ) : null}
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input
+            // className="formInput"
+            type="text"
+            name="user_name"
+            {...register("user_name")}
+          ></input>
+          {errors.user_email && <p style={styleObj}>This field is required</p>}
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input
+            // className="formInput"
+            type="text"
+            name="user_email"
+            {...register("user_email", { required: true })}
+          ></input>
+        </div>
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea
+            // className="formInput2"
+            name="Text1"
+            cols="30"
+            rows="5"
+            name="message"
+            {...register("message")}
+            maxLength="1500"
+          ></textarea>
+          {message.length !== 0 ? (
+            <p className="message-chars-left">
+              {`${500 - message.length} chars left`}
+            </p>
+          ) : null}
+        </div>
 
         <input className="formButton" type="submit" value="Send" />
       </form>
--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,8 +1,8 @@
 import "./MainContent.css";
 import profilepic from "../Images/pp.svg";
-import frontpagepng from "../Images/frontpage.png"
+import frontpagepng from "../Images/frontpage.png";
 import ParticleBackground from "./ParticleBackground";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Work from "./Work/Work";
 import Xmeme from "./../Images/x-meme.PNG";
 import Clone1 from "./../Images/clone.PNG";
@@ -18,10 +18,17 @@ import mongoPng from "./../Images/mongodb.png";
 import nferPng from "./../Images/nference.png";
 import abPng from "./../Images/abinbev.jpg";
 
-
 const MainContent = () => {
   const [cardAnime, setCardAnime] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   console.log(cardAnime);
+
+  useEffect(() => {
+    // Simulate loading delay
+    setTimeout(() => {
+      setLoaded(true);
+    }, 10); // Adjust the delay as needed
+  }, []);
 
   const handleAnimate = () => {
     setCardAnime(!cardAnime);
@@ -33,48 +40,81 @@ const MainContent = () => {
       <Route path="/">
         <div className="sectionContainer">
           <section id="section1">
-              <div className="section1-inner-container">
-              <img id="section1-img" src = {frontpagepng}></img>
-              </div>
-              <div  className="section1-inner-container">
-                <div id="section1-inner-text">
-                  <h2 id="section1-text-header">Hey y'all</h2>
-                  <div >
-                    <p>
-                      I'm an 2021 Engineering Graduate. Working as a FS developer.<br/>
-                      I also do <stong>freelancing</stong>. Do checkout my projects.
-
-                    </p>
+            <div
+              className={`section1-inner-container ${loaded ? "loaded" : ""}`}
+            >
+              <img
+                className="section1-animate"
+                id="section1-img"
+                src={frontpagepng}
+              ></img>
+            </div>
+            <div
+              className={`section1-inner-container ${loaded ? "loaded" : ""}`}
+            >
+              <div id="section1-inner-text">
+                <h2 className="section1-animate" id="section1-text-header">
+                  Hey y'all
+                </h2>
+                <div className="section1-animate">
+                  <p>
+                    I'm an 2021 Engineering Graduate. Working as a FS developer.
+                    <br />I also do <stong>freelancing</stong>. Do checkout my
+                    projects.
+                  </p>
+                </div>
+                <div className="section1-animate">
+                  Tech stack
+                  <div className="section1-logo-outer">
+                    <div className="section1-logo-container">
+                      <img
+                        id="section1-img"
+                        height={50}
+                        width={100}
+                        src={nodePng}
+                      ></img>
+                    </div>
+                    <div className="section1-logo-container">
+                      <img
+                        id="section1-img"
+                        height={50}
+                        width={100}
+                        src={reactPng}
+                      ></img>
+                    </div>
+                    <div className="section1-logo-container">
+                      <img
+                        id="section1-img"
+                        height={50}
+                        width={100}
+                        src={mongoPng}
+                      ></img>
+                    </div>
                   </div>
-                  <div>
-                      Tech stack
-                      <div className="section1-logo-outer">
-                        <div className="section1-logo-container">
-                            <img id="section1-img" height= {50} width= {100} src = {nodePng}></img>
-                        </div>
-                        <div className="section1-logo-container">
-                          <img id="section1-img"  height= {50} width= {100} src = {reactPng}></img>
-                        </div>
-                        <div className="section1-logo-container">
-                          <img id="section1-img"  height= {50} width= {100} src = {mongoPng}></img>
-                        </div>
-                      </div>
-
-                  </div>
-                  <div>
-                      Worked At
-                      <div className="section1-logo-outer">
-                        <div className="section1-logo-container">
-                          <img id="section1-img"  height= {50} width= {100} src = {nferPng}></img>
-                        </div>
-                        <div className="section1-logo-container">
-                          <img id="section1-img"  height= {50} width= {100} src = {abPng}></img>
-                        </div>
-                      </div>
-
+                </div>
+                <div className="section1-animate">
+                  Worked At
+                  <div className="section1-logo-outer">
+                    <div className="section1-logo-container">
+                      <img
+                        id="section1-img"
+                        height={50}
+                        width={100}
+                        src={nferPng}
+                      ></img>
+                    </div>
+                    <div className="section1-logo-container">
+                      <img
+                        id="section1-img"
+                        height={50}
+                        width={100}
+                        src={abPng}
+                      ></img>
+                    </div>
                   </div>
                 </div>
               </div>
+            </div>
           </section>
 
           <section id="section2">
@@ -103,7 +143,7 @@ const MainContent = () => {
           </section>
 
           <section id="section3">
-            <div className={`frontCard ${cardAnime ? "cardAnime" : null}`}>
+            {/* <div className={`frontCard ${cardAnime ? "cardAnime" : null}`}>
               <ion-icon
                 className="frontCardLogo"
                 name="code-working-outline"
@@ -112,7 +152,7 @@ const MainContent = () => {
               <div className="clickContact" onClick={handleAnimate}>
                 Click <ion-icon name="flash-outline"></ion-icon> here{" "}
               </div>
-            </div>
+            </div> */}
             <div className="messageOuterContainer">
               <div className="messageText">
                 <h1>Let's</h1>
