ecea8b5899856a7aee72debcdeade7cd728790ae
kibi11 <arpansinghranchi@gmail.com>
2024-04-25 00:25:46 +0530

    fixed scroll

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,8 +5,23 @@ import Resume from "./Work/Resume";
 
 const Header = () => {
   const [responsive, setResponsive] = useState(false);
-  const handleResponsive = () => {
+  const handleResponsive = (sectionID) => {
     setResponsive(!responsive);
+    console.log(sectionID);
+    const element = document.getElementById(sectionID);
+    if (element && sectionID) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleScrollToSection = (sectionID) => {
+    console.log(sectionID);
+    const element = document.getElementById(sectionID);
+    if (element && sectionID) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
@@ -21,28 +36,38 @@ const Header = () => {
         <div></div>
       </div>
       <div id="overlay" className={responsive ? "menu" : null}>
-        <Link to="/Home" onClick={handleResponsive}>
+        <Link to="/Home" onClick={() => handleResponsive("section1")}>
           Home
         </Link>
-        <Link to="/section2" exact onClick={handleResponsive}>
+        <Link to="/Work" exact onClick={() => handleResponsive("section2")}>
           Work
         </Link>
-        <Link to="/section3" onClick={handleResponsive}>
+        <Link to="/Contact" onClick={() => handleResponsive("section3")}>
           Contact Me
         </Link>
-        <Link to="/Resume" onClick={handleResponsive}>Resume</Link>
+        <Link to="/Resume" onClick={handleResponsive}>
+          Resume
+        </Link>
       </div>
       <ul className="navItems">
         <li className="navItem">
-          <Link to="/Home">Home</Link>
+          <Link to="/Home" onClick={() => handleScrollToSection("section1")}>
+            Home
+          </Link>
         </li>
         <li className="navItem">
-          <Link to="/#section2" exact>
+          <Link
+            to="/Work"
+            onClick={() => handleScrollToSection("section2")}
+            exact
+          >
             Work
           </Link>
         </li>
         <li className="navItem">
-          <Link to="/#section3">Contact</Link>
+          <Link to="/Contact" onClick={() => handleResponsive("section3")}>
+            Contact
+          </Link>
         </li>
         <li className="navItem">
           <Link to="/Resume">Resume</Link>
